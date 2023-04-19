f7cf8602de6653e43e9f3d6c83251bff1d4dd497
GUstavoDMarco <gustavo.marco@unesp.br>
2023-04-19 12:39:03 -0300

    Criando outra conta

--- src/Demo.java
+++ src/Demo.java
@@ -10,5 +10,17 @@ public class Demo{
         c1.sacar(650);
         c1.sacar(630);
         c1.fecharConta();
+
+        System.out.println("\n");
+
+        Conta c2 = new Conta();
+        c2.pagarMensal();
+        c2.abrirConta("CC","Ana");
+        c2.depositar(500);
+        c2.pagarMensal();
+        c2.fecharConta();
+        c2.sacar(650);
+        c2.sacar(538);
+        c2.fecharConta();
     }
 }
